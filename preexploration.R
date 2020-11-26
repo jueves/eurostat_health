2a05345dc43f3b430817a3b653e1ed6fa6663ada
Luis Cobiella Hernández <alu0100358797@ull.edu.es>
2020-11-26 12:16:12 +0000

    Added new metadata and minnor changes.

--- preexploration.R
+++ preexploration.R
@@ -1,12 +1,7 @@
-# Esto no lo he comprobado, pero debería de funcionar
-# Con reticulate Link: https://www.r-bloggers.com/2018/03/reticulate-r-interface-to-python/
-source_python('ETL.py')
-(data1, metadata1) <- import_eurostat_dataset(file_name)
+library(reticulate)
+library(ggplot2)
 
-data.expen.hc <- read.csv('data/hlth_sha11_hc.tsv', sep = '\t', na.strings=': ')
-data.beds <- read.csv('data/hlth_sha11_hc.tsv', sep = '\t', na.strings=': ')
-# Creo que es un cubo, y la primera columna un comma separated time. Es decir, 
-# es como un csv, en el que la casilla de los años para cada observación es un
-# vector de valores para cada año.
+source_python('download.py')
+source_python('transform.py')
+staff.all <- import_eurostat_dataset('data/staff_all.gz')
 
-data.deaths <- read.csv('data/estand_deaths_per_region.csv', na.strings=': ')
