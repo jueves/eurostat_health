95bc1cba84c6108d7d00d9e58e6af5040f6b02a9
Luis Cobiella Hernández <alu0100358797@ull.edu.es>
2020-12-09 13:30:37 +0000

    Adds nuts.json creation

--- prepare_metadata.py
+++ prepare_metadata.py
@@ -29,6 +29,35 @@ else:
   with open(nuts_file_name, 'w') as f:
     json_dic = json.dumps(dic_nuts, indent=4)
     f.write(json_dic)
+
+
+#############
+# nuts.json #
+#############
+# Creates json file all NUTS codes.
+
+nuts_url = 'https://ec.europa.eu/eurostat/documents/345175/629341/NUTS2021.xlsx'
+nuts_file_name = 'data/nuts.json'
+
+if os.path.isfile(nuts_file_name):
+  print(nuts_file_name, 'is already downloaded.')
+else:
+  nuts = pd.read_excel(nuts_url, sheet_name='NUTS & SR 2021')
+  
+  dic_nuts = {0:dict(), 1:dict(), 2:dict(), 3:dict()}
+  for index, row in nuts.iterrows():
+    if not pd.isna(row["NUTS level"]):
+      level = int(row["NUTS level"])
+      code = row['Code 2021']
+      name = row[1+level]
+      dic_nuts[level][code] = name
+
+  
+  # Export dictionary to json
+  with open(nuts_file_name, 'w') as f:
+    json_dic = json.dumps(dic_nuts, indent=4)
+    f.write(json_dic)
+
     
 ####################
 # icd10_v2012.json #
@@ -69,13 +98,15 @@ else:
   icd10 = json.loads(icd10_req.content)
   
   # Process file
-  icd10_v2007_dic = {'names': dict(), 'levels': dict()}
+  icd10_v2007_dic = {'names': dict(), 'levels': dict(),
+                     'inverse_levels': {0:list(), 1:list(), 2:list(), 3:list()}}
   for element in icd10['concepts']:
     icd10_v2007_dic['names'][element['Notation']] = element['Label']
 
   # Add levels from ICD10 v2007
   for index, row in icd10_df.iterrows():
     icd10_v2007_dic['levels'][row.ICD_10_edited] = row.LEVEL
+    icd10_v2007_dic['inverse_levels'][row.LEVEL].append(row.ICD_10_edited)
     
   # Export to json
   with open('data/icd10_v2007.json', 'w') as f:
