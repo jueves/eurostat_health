0dc0876ca13d213fecc7dc7893fc3a7b6a151b22
jueves <martes1+varios@fastmail.fm>
2024-04-24 10:00:40 +0100

    Specify package versions for docker image
    For R, the latest version of package of "remotes" needs to be
    installed, as this is needed to specify a R package version.

--- docker/docker_src/install_packages.R
+++ docker/docker_src/install_packages.R
@@ -1,4 +1,6 @@
 # Script de instalación para el Dockerfile
-install.packages("plotly")
+install.packages("remotes")
+library("remotes")
+install_version("plotly", "4.10.4")
 library(reticulate)
-py_install(c("pandas", "requests", "openpyxl"))
+py_install(c("pandas==2.2.2", "requests==2.31.0", "openpyxl==3.1.2"))
